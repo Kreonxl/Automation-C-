06b3a8fe1303082452e9495845bbcd2968c1646f
Krasimir Kuzmanov <kkuzmanov78@gmail.com>
2024-11-15 10:08:05 +0200

    1st commit

--- Primitives/Program.cs
+++ Primitives/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Primitives
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    
+
+}
+
 
